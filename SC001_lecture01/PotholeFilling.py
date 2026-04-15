2d6ae7cd559dbc37a3d0c9afc322b260d50243db
yuyuxian007 <wang20051002@gmail.com>
2026-04-15 19:48:31 +0800

    Add files via upload

---
+++ SC001_lecture01/PotholeFilling.py
@@ -0,0 +1,46 @@
+"""
+File: PotholeFilling.py
+Name: Haoyu Wang
+--------------------------
+This program demonstrates how Karel fills multiple potholes
+by using decomposition.
+
+In this program, we will guide Karel to fix three potholes
+on the road. Instead of writing all the commands in one place,
+we will practice breaking a big task into smaller, reusable
+functions to make the program clearer and easier to manage.
+"""
+
+from karel.stanfordkarel import *
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def put_beeper99():
+    for i in range(99):
+        put_beeper()
+def run():
+    for i in range(3):
+        move()
+        turn_right()
+        move()
+        put_beeper99()
+        turn_left()
+        turn_left()
+        move()
+        turn_right()
+        move()
+
+
+def main():
+    """
+    Haoyu Wang
+    """
+    run()
+
+
+
+
+# ----- DO NOT EDIT CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
